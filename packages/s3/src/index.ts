0c443a5bf12bba95fa6d7e6631b225c65110fc37
Cristian Magaña <33166233+cristianmagana@users.noreply.github.com>
2026-06-09 05:06:58 -0700

    feat(s3): scaffold workspace package

---
+++ packages/s3/src/index.ts
@@ -0,0 +1,9 @@
+export interface S3PackageInfo {
+  readonly packageName: '@cdk-construct/s3';
+  readonly service: 'Amazon S3';
+}
+
+export const s3PackageInfo: S3PackageInfo = {
+  packageName: '@cdk-construct/s3',
+  service: 'Amazon S3',
+};

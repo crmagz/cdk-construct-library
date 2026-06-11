c7520673951e29633001e6b13e673b3c3b7302b5
Cristian Magaña <33166233+cristianmagana@users.noreply.github.com>
2026-06-10 17:38:29 -0700

    feat(cloudfront): scaffold workspace package

---
+++ packages/cloudfront/src/index.ts
@@ -0,0 +1,9 @@
+export interface CloudfrontPackageInfo {
+  readonly packageName: '@cdk-construct/cloudfront';
+  readonly service: 'Amazon CloudFront';
+}
+
+export const cloudfrontPackageInfo: CloudfrontPackageInfo = {
+  packageName: '@cdk-construct/cloudfront',
+  service: 'Amazon CloudFront',
+};

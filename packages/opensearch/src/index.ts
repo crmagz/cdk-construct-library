f58a2d6d117301be6bbf5f890376f0004b9eb499
Cristian Magaña <33166233+cristianmagana@users.noreply.github.com>
2026-06-19 18:14:12 -0700

    feat(opensearch): add domain construct

--- packages/opensearch/src/index.ts
+++ packages/opensearch/src/index.ts
@@ -1,9 +1,2 @@
-export interface OpenSearchPackageInfo {
-  readonly packageName: '@cdk-construct/opensearch';
-  readonly service: 'Amazon OpenSearch Service';
-}
-
-export const openSearchPackageInfo: OpenSearchPackageInfo = {
-  packageName: '@cdk-construct/opensearch',
-  service: 'Amazon OpenSearch Service',
-};
+export * from './domain.js';
+export * from './types.js';

6d3a3cf05991afa3444f6e5aede59707831db7da
@crmagz <whudduptho@gmail.com>
2026-06-21 19:32:23 -0700

    feat(eks): add release preview marker

---
+++ .changeset/gentle-eks-feature.md
@@ -0,0 +1,5 @@
+---
+'@cdk-construct/eks': minor
+---
+
+Add a release preview feature marker for the EKS package.
--- packages/eks/src/index.ts
+++ packages/eks/src/index.ts
@@ -1,9 +1,11 @@
 export interface EksPackageInfo {
   readonly packageName: '@cdk-construct/eks';
   readonly service: 'Amazon EKS';
+  readonly releasePreview: boolean;
 }
 
 export const eksPackageInfo: EksPackageInfo = {
   packageName: '@cdk-construct/eks',
   service: 'Amazon EKS',
+  releasePreview: true,
 };

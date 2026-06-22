ffc8ff911b2eb871c3a101a609781ca98f5fc762
@crmagz <whudduptho@gmail.com>
2026-06-21 20:18:13 -0700

    feat(eks): expose conventional release-note metadata

---
+++ .changeset/eks-conventional-release-notes.md
@@ -0,0 +1,5 @@
+---
+'@cdk-construct/eks': minor
+---
+
+feat(eks): expose conventional release-note metadata
--- packages/eks/src/index.ts
+++ packages/eks/src/index.ts
@@ -1,11 +1,13 @@
-export interface EksPackageInfo {
+export type EksPackageInfo = {
   readonly packageName: '@cdk-construct/eks';
   readonly service: string;
   readonly releasePreview: boolean;
-}
+  readonly releaseNoteFormat: 'conventional-commits';
+};
 
 export const eksPackageInfo: EksPackageInfo = Object.freeze({
   packageName: '@cdk-construct/eks',
   service: 'Amazon Elastic Kubernetes Service',
   releasePreview: true,
+  releaseNoteFormat: 'conventional-commits',
 });
